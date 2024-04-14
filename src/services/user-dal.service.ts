2a76a053f84d57c80114b10ec84dd14e087bb965
Noah-McC <166862144+Noah-McC@users.noreply.github.com>
2024-04-14 12:14:18 -0400

    Updated user-dal.service.ts
    Adding insert and select all functions to the user dal. the dal is not finished yet. will update once it is finished

--- src/services/user-dal.service.ts
+++ src/services/user-dal.service.ts
@@ -1,9 +1,66 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {DatabaseService} from "./database.service";
+import {User} from "../models/User.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserDalService {
 
-  constructor() { }
+  database = inject(DatabaseService)
+
+  constructor() {
+
+  }
+
+  insert(user: User): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const transaction = this.database.db.transaction(["users"], "readwrite");
+
+      transaction.oncomplete = (event: any) => {
+        console.log("Success: insert transaction successful");
+      };
+      transaction.onerror = (event: any) => {
+        console.log("Error: error in insert transaction: " + event);
+      };
+
+      const usersStore = transaction.objectStore("users");
+      const req = usersStore.add(user);
+
+      req.onsuccess = (event: any) => {
+        console.log(`Success: user added successfully ${event.target.result}`);
+        resolve(event.target.result);
+      };
+
+      req.onerror = (event: any) => {
+        console.log("Error: error in add: " + event);
+        reject(event);
+      };
+    });
+  }
+
+  selectAll(): Promise<User[]> {
+    return new Promise((resolve, reject) => {
+      const transaction = this.database.db.transaction(["users"]);
+
+      transaction.oncomplete = (event: any) => {
+        console.log("Success: selectAll transaction successful");
+      };
+      transaction.onerror = (event: any) => {
+        console.log("Error: error in selectAll transaction: " + event);
+      };
+
+      const usersStore = transaction.objectStore("users");
+      const req = usersStore.getAll();
+
+      req.onsuccess = (event: any) => {
+        resolve(event.target.result);
+      };
+      req.onerror = (event: any) => {
+        reject(event);
+      };
+
+    });
+  }
+
 }
